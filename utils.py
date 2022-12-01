0889891046aed541ec9d8959c2c117eccce1eba3
Brandon Morgan <59944082+OUStudent@users.noreply.github.com>
2022-12-01 11:08:50 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,28 +7,20 @@ import plotly.graph_objects as go
 import numpy as np
 from datetime import datetime
 import time
-
+import scipy
 
 def create_data_on_disk(graph_size, num_samples, is_save=True, filename=None, is_return=False, seed=1234):
     """Generate validation dataset (with SEED) and save
     """
 
-    CAPACITIES = {
-        10: 20.,
-        20: 30.,
-        50: 40.,
-        100: 50.
-    }
-    depo, graphs, demand = (tf.random.uniform(minval=0, maxval=1, shape=(num_samples, 2), seed=seed),
-                            tf.random.uniform(minval=0, maxval=1, shape=(num_samples, graph_size, 2), seed=seed),
-                            tf.cast(tf.random.uniform(minval=1, maxval=10, shape=(num_samples, graph_size),
-                                                      dtype=tf.int32, seed=seed), tf.float32) / tf.cast(CAPACITIES[graph_size], tf.float32)
-                            )
+    graphs = tf.random.uniform(minval=0, maxval=1, shape=(num_samples, graph_size + 2, 2))
+    costs = tf.convert_to_tensor([scipy.spatial.distance.cdist(g, g, "euclidean") for g in graphs])
+
     if is_save:
-        save_to_pickle('Validation_dataset_{}.pkl'.format(filename), (depo, graphs, demand))
+        save_to_pickle('Validation_dataset_{}.pkl'.format(filename), (graphs, costs))
 
     if is_return:
-        return tf.data.Dataset.from_tensor_slices((list(depo), list(graphs), list(demand)))
+        return tf.data.Dataset.from_tensor_slices((list(graphs), list(costs)))
 
 
 def save_to_pickle(filename, item):
@@ -51,11 +43,11 @@ def read_from_pickle(path, return_tf_data_set=True, num_samples=None):
                 break
     objects = objects[0]
     if return_tf_data_set:
-        depo, graphs, demand = objects
+        start, end, graphs, costs = objects
         if num_samples is not None:
-            return tf.data.Dataset.from_tensor_slices((list(depo), list(graphs), list(demand))).take(num_samples)
+            return tf.data.Dataset.from_tensor_slices((list(start), list(end), list(graphs), list(costs))).take(num_samples)
         else:
-            return tf.data.Dataset.from_tensor_slices((list(depo), list(graphs), list(demand)))
+            return tf.data.Dataset.from_tensor_slices((list(start), list(end), list(graphs), list(costs)))
     else:
         return objects
 
@@ -64,19 +56,11 @@ def generate_data_onfly(num_samples=10000, graph_size=20):
     """Generate temp dataset in memory
     """
 
-    CAPACITIES = {
-        10: 20.,
-        20: 30.,
-        50: 40.,
-        100: 50.
-    }
-    depo, graphs, demand = (tf.random.uniform(minval=0, maxval=1, shape=(num_samples, 2)),
-                            tf.random.uniform(minval=0, maxval=1, shape=(num_samples, graph_size, 2)),
-                            tf.cast(tf.random.uniform(minval=1, maxval=10, shape=(num_samples, graph_size),
-                                                      dtype=tf.int32), tf.float32)/tf.cast(CAPACITIES[graph_size], tf.float32)
-                            )
+    graphs = tf.random.uniform(minval=0, maxval=1, shape=(num_samples, graph_size + 2, 2))
+    costs = tf.convert_to_tensor([scipy.spatial.distance.cdist(g, g, "euclidean") for g in graphs])
 
-    return tf.data.Dataset.from_tensor_slices((list(depo), list(graphs), list(demand)))
+
+    return tf.data.Dataset.from_tensor_slices((list(graphs), list(costs)))
 
 
 def get_results(train_loss_results, train_cost_results, val_cost, save_results=True, filename=None, plots=True):
